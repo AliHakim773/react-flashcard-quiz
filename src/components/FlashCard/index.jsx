f11152c081327ee2e387c73c947c03c45e055ad2
AliHakim773 <alihakim7732@gmail.com>
2023-11-28 16:25:09 +0200

    added flashcard basic html

--- src/components/FlashCard/index.jsx
+++ src/components/FlashCard/index.jsx
@@ -4,10 +4,19 @@ function FlashCard({ flashCard }) {
     const [flip, setFlip] = useState(false)
     return (
         <div
+            className={`card ${flip ? "flip" : ""}`}
             onClick={() => {
                 setFlip(!flip)
             }}>
-            {flip ? flashCard.answer : flashCard.question}
+            <div className='front'>
+                {flashCard.question}
+                <div className='flashcard-options'>
+                    {flashCard.options.map((option) => (
+                        <div className='flashcard-option'>{option}</div>
+                    ))}
+                </div>
+            </div>
+            <div className='back'>{flashCard.answer}</div>
         </div>
     )
 }
