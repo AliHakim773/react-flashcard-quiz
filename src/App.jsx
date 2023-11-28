2c4deb0d55eeea9acf92b9deaf51a1b9f8da7208
AliHakim773 <alihakim7732@gmail.com>
2023-11-28 16:11:54 +0200

    fixed src structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react"
 import "./styles/index.css"
+import FlashCardList from "./components/FlashCradList"
 
 function App() {
-    return <div className='app'></div>
+    const [flashCards, setFlashCards] = useState(SAMPLE_FLASHCARDS)
+    return (
+        <div className='app'>
+            <FlashCardList flashCards={flashCards} />
+        </div>
+    )
 }
+
+const SAMPLE_FLASHCARDS = [
+    {
+        id: 1,
+        question: "what is 2 + 2",
+        answer: "4",
+        options: ["2", "3", "4", "5"],
+    },
+    {
+        id: 2,
+        question: "Question 2",
+        answer: "answer",
+        options: ["answer", "answer 1", "answer 2", "answer 3"],
+    },
+    {
+        id: 3,
+        question: "Question 3",
+        answer: "answer",
+        options: ["answer", "answer 1", "answer 2", "answer 3"],
+    },
+]
 
 export default App
---
+++ src/components/FlashCradList/index.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./styles.css"
+
+function FlashCardList({ flashCards }) {
+    return <div className='card-grid'></div>
+}
+
+export default FlashCardList
