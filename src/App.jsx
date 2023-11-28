5a74c262acb264835bd0cce3b8f0c294e3847b5d
AliHakim773 <alihakim7732@gmail.com>
2023-11-28 20:38:45 +0200

    removed Form compenent and added the form directly in app.jsx --to be fixed with redux

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,61 @@ import { useEffect, useRef, useState } from "react"
 import FlashCardList from "./components/FlashCradList"
 
 import { get_questions } from "./core/api/get_answers"
+import { get_options } from "./core/api/get_options"
 
 import "./styles/index.css"
-import Form from "./components/Form"
 
 function App() {
+    const categoryEl = useRef()
+    const amountEl = useRef()
+
     const [flashCards, setFlashCards] = useState([])
+    const [categories, setCategories] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
 
     useEffect(() => {
         const getQuestions = async () => {
             const result = await get_questions()
             setFlashCards(result)
+            const getCategories = await get_options()
+            setCategories(getCategories)
         }
         getQuestions()
     }, [])
 
     return (
         <div className='app'>
-            <Form />
+            <form className='header' onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <label htmlFor='category'>Category</label>
+                    <select id='category' ref={categoryEl}>
+                        {categories?.map((category) => {
+                            return (
+                                <option key={category.id} value={category.id}>
+                                    {category.name}
+                                </option>
+                            )
+                        })}
+                    </select>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='amount'>Number of questions</label>
+                    <input
+                        type='number'
+                        id='amount'
+                        min='1'
+                        step='1'
+                        defaultValue={10}
+                        ref={amountEl}
+                    />
+                </div>
+                <div className='form-group'>
+                    <button className='btn'>Generate</button>
+                </div>
+            </form>
             <div className='container'>
                 <FlashCardList flashCards={flashCards} />
             </div>
