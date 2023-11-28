c7b6f6e205946f72921bf7b4c9c4b1ebef6874a5
AliHakim773 <alihakim7732@gmail.com>
2023-11-28 20:22:12 +0200

    added form component

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,22 @@ import FlashCardList from "./components/FlashCradList"
 import { get_questions } from "./core/api/get_answers"
 
 import "./styles/index.css"
-import { get_options } from "./core/api/get_options"
+import Form from "./components/Form"
 
 function App() {
-    const categoryEl = useRef()
-
     const [flashCards, setFlashCards] = useState([])
-    const [categories, setCategories] = useState([])
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-    }
 
     useEffect(() => {
         const getQuestions = async () => {
             const result = await get_questions()
             setFlashCards(result)
-            const categories = await get_options()
-            setCategories(categories)
         }
         getQuestions()
     }, [])
 
     return (
         <div className='app'>
-            <form className='header' onSubmit={handleSubmit}>
-                <div className='form-group'>
-                    <label htmlFor='category'>Category</label>
-                    <select id='category' ref={categoryEl}></select>
-                </div>
-            </form>
+            <Form />
             <div className='container'>
                 <FlashCardList flashCards={flashCards} />
             </div>
